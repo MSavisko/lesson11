55c722abcebfb4c7ba741abdd7eb5b72fee0b148
Maksym Savisko <stowyn@gmail.com>
2016-01-30 20:08:56 +0200

    Task #1. Add calculation of Fibonacci numbers and init table view

---
+++ Fibonacci/Fibonacci/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Fibonacci
+//
+//  Created by Maksym Savisko on 1/30/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
