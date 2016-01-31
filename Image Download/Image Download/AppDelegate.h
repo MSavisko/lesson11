203735994fb72cbbc8c654c5b13dcfb3734b34f1
Maksym Savisko <stowyn@gmail.com>
2016-01-31 12:45:56 +0200

    Task #3. Init

---
+++ Image Download/Image Download/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Image Download
+//
+//  Created by Maksym Savisko on 1/31/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Image Download/Image Download/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Image Download
+//
+//  Created by Maksym Savisko on 1/31/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
